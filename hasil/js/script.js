5bba248387052d373754ab508bdbaab68fdc48c0
wpumb <165593993+wpumb@users.noreply.github.com>
2024-04-01 03:33:37 +0700

    Add files via upload

---
+++ hasil/js/script.js
@@ -0,0 +1,5 @@
+const namaPerson1 = document.getElementById('np1')
+const namaAnggota1 = document.getElementById('na1')
+
+namaPerson1.innerHTML = "Andre Yudoharyanto"
+namaAnggota1.innerHTML = namaPerson1.textContent
