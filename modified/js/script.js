7ba996cb0cddc94a88cb693ec86546bc6f032574
wpumb <165593993+wpumb@users.noreply.github.com>
2024-04-01 08:35:23 +0700

    -

--- modified/js/script.js
+++
@@ -1,5 +0,0 @@
-const namaPerson1 = document.getElementById('np1')
-const namaAnggota1 = document.getElementById('na1')
-
-namaPerson1.innerHTML = "Andre Yudoharyanto"
-namaAnggota1.innerHTML = namaPerson1.textContent
